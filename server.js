ac8244b064581e1795f6880d128353a197826e51
Owen Clinton <owenwjc@gmail.com>
2020-10-03 20:19:03 -0700

    Simple CRUD implementation

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const http = require('http');
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
 const app = express();
-app.use(express.json());
-app.use(express.static("express"));
-// default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
-console.debug('Server listening on port ' + port);
+
+MongoClient.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false',{
+  useUnifiedTopology: true}).then(client =>{
+    console.log('In the mainframe')
+    const db = client.db('db')
+    const testCollection = db.collection('Test Collection')
+    app.use(bodyParser.urlencoded({extended:true}))
+
+    app.listen(3000,function(){
+      console.log('listening on 3000')
+    })
+
+    app.get('/',function(req,res){
+      res.sendFile(__dirname + '/express' + '/index.html')
+    })
+
+    app.post('/direction', (req,res) => {
+      var obj = {button: req.body.button, name: req.body.name}
+      testCollection.insertOne(obj).then(
+        result => {
+          res.redirect('/')
+        }).catch(
+          error => console.error(error)
+        )
+    })
+  }
+).catch(console.error)
