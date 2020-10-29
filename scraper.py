48978010a2bca42a113f6f24e3a53a5258570f96
Owen Clinton <owenwjc@gmail.com>
2020-10-29 11:14:32 -0700

    fixed error in scraper and added neutral to labelling

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ import time
 import datetime
 import re
 import nltk
+import numpy as np
 
 from datetime import datetime, timedelta
 from pymongo import MongoClient
@@ -52,7 +53,7 @@ def pullTickers(string):
 
 notTickers = {'DCF', 'IMO', 'CAN', 'MMS', 'ARE', 'CDC', 'NEW', 'LOVE', 'NYC', 'CASH', 'AI', 
 'NAV', 'GOOD', 'DD', 'ATH', 'APPS', 'EDIT', 'WOW', 'PCB', 'UNIT', 'TA', 'VG', 'SELF', 'MR',
-'RARE', 'ALEX', 'KEY', 'STIM', 'GO', 'SEE', 'CFO', 'CAL', 'JOE', 'REV', 'PE', 'CHI', 'EVE', 
+'RARE', 'ALEX', 'KEY', 'STIM', 'GO', 'SEE', 'CFO', 'CAL', 'REV', 'PE', 'CHI', 'EVE', 'PDT',
 'CO', 'EV', 'TTM', 'EOD', 'AT', 'HUGE', 'ES', 'ONE', 'PT', 'CEO', 'ZEN', 'NOW', 'JAN', 'O',
 'OR', 'PG', 'ROCK', 'FOUR', 'ONE', 'TWO', 'FIVE', 'SIX', 'NINE', 'TEN', 'ON', 'SU', 'XT',
 'WELL', 'NOV', 'MAR', 'JAN', 'FUN', 'NOW', 'VERY'}
