711b5c09feb9f01ec6ef24a2f4a4ec1a50e9106a
Owen Clinton <owenwjc@gmail.com>
2020-11-08 17:20:30 -0800

    changed for linux

--- checkpoint.py
+++ checkpoint.py
@@ -6,16 +6,11 @@ from flair.embeddings import WordEmbeddings, FlairEmbeddings, DocumentRNNEmbeddi
 from flair.trainers import ModelTrainer
 from pathlib import Path
 
-def main():
-
-    corpus = ClassificationCorpus(Path('data/'), test_file='test.csv', dev_file='dev.csv', train_file='train.csv')
-    checkpoint = 'model/checkpoint.pt'
-    trainer = ModelTrainer.load_checkpoint(checkpoint, corpus)
-    trainer.train('model/',
-                  max_epochs=100,
-                  embeddings_storage_mode='none',
-                  patience=5,
-                  checkpoint=True)
-    
-if __name__ == "__main__":
-    main()
+corpus = ClassificationCorpus(Path('data/'), test_file='test.csv', dev_file='dev.csv', train_file='train.csv')
+checkpoint = 'model/checkpoint.pt'
+trainer = ModelTrainer.load_checkpoint(checkpoint, corpus)
+trainer.train('model/',
+                max_epochs=100,
+                embeddings_storage_mode='none',
+                patience=5,
+                checkpoint=True)
